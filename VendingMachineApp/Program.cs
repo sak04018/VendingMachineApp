d73c77dd7e8d8bb75ab6ecceff496420acb4daf1
Saad Khokhar <sakhokha@microsoft.com>
2018-10-11 16:09:35 -0700

    App classes

---
+++ VendingMachineApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VendingMachineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************");
+            Console.WriteLine("Welcome to our YumYum vending machine!");
+            Console.WriteLine("Please make a selection from the following menu options");
+        }
+    }
+}
